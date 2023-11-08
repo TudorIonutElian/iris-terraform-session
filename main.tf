4a5145e6d3074538eb5cfc692a1e9ce1fc0b67cd
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-08 23:29:19 +0200

    Make big changes

--- main.tf
+++ main.tf
@@ -16,6 +16,44 @@ provider "aws" {
   profile = "default"
 }
 
+# Add VPC
+resource "aws_vpc" "iris_terraform_demo_vpc" {
+  cidr_block = "10.0.0.0/16" # Replace with your desired CIDR block
+  tags = {
+    Name = "Prod_VPC"
+  }
+}
+
+resource "aws_internet_gateway" "iris_terraform_demo_gw" {
+  vpc_id = aws_vpc.iris_terraform_demo_vpc.id
+}
+
+resource "aws_route_table" "iris_terraform_demo_route_table" {
+  vpc_id = aws_vpc.iris_terraform_demo_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.iris_terraform_demo_gw.id
+  }
+  tags = {
+    Name = "prod-RT"
+  }
+}
+
+resource "aws_subnet" "iris_terraform_demo_subnet" {
+  vpc_id            = aws_vpc.iris_terraform_demo_vpc.id
+  cidr_block        = "10.0.1.0/24" # Replace with your desired CIDR block
+  availability_zone = "eu-central-1"
+  tags = {
+    Name = "prod_subnet"
+  }
+}
+
+resource "aws_route_table_association" "iris_terraform_demo_route_table_asso" {
+  subnet_id      = aws_subnet.iris_terraform_demo_subnet.id
+  route_table_id = aws_route_table.iris_terraform_demo_route_table.id
+}
+
 # Added data source to filter the ami
 data "aws_ami" "iris_ec2_ami_filter" {
   owners = ["amazon"]
@@ -40,23 +78,38 @@ resource "aws_key_pair" "iris_terraform_demo_key" {
 }
 
 resource "aws_security_group" "web_security_group" {
-  name = "web_security_group"
-  description = "Configuration for allowing traffic for 22 and 80 port"
+  name        = "webserver_sg"
+  description = "Allow inbound SSH and HTTP traffic"
+  vpc_id      = aws_vpc.demo_vpc.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Web-traffic"
+  }
 }
 
 # Adding ec2 instance configuration
--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "iris_ec2_instance_ami_filter" {
     description = "I will print AMI ID"
     value = data.aws_ami.iris_ec2_ami_filter.id
 }
+
+output "iris_terraform_demo_public_ip" {
+  value = aws_instance.iris_ec2_instance_demo.public_ip
+}
