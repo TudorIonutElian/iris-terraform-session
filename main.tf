6d08dc7063602feb2e080060bf18b6cd90a2f1f7
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-09 08:42:56 +0200

    Remove unecessary

--- main.tf
+++ main.tf
@@ -25,82 +25,6 @@ resource "aws_vpc" "app_vpc" {
   }
 }
 
-# Added data source to filter the ami
-data "aws_ami" "iris_ec2_ami_filter" {
-  owners = ["amazon"]
-  most_recent = true
-
-  filter {
-    name = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-
-  filter {
-    name = "architecture"
-    values = ["x86_64"]
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  tags = {
-    Name = "vpc_igw"
-  }
-}
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone = "eu-central-1a"
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public_rt"
-  }
-}
-
-resource "aws_route_table_association" "public_rt_asso" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_instance" "web" {
-  ami = data.aws_ami.iris_ec2_ami_filter.id
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.sg.id]
-
-  user_data = <<-EOF
-  #!/bin/bash
-  sudo apt update -y
-  sudo apt install apache2 -y
-  sudo systemctl start apache2
-  echo "Deploy a web server on aws" | sudo tee /var/www/html/index.html
-  EOF
-
-  tags = {
-    Name = "web_instance"
-  }
-
-  volume_tags = {
-    Name = "web_instance"
-  } 
-}
-
 resource "aws_security_group" "sg" {
   name        = "allow_ssh_http"
   description = "Allow ssh http inbound traffic"
@@ -135,4 +59,46 @@ resource "aws_security_group" "sg" {
   tags = {
     Name = "allow_ssh_http"
   }
+}
+
+# Added data source to filter the ami
+data "aws_ami" "iris_ec2_ami_filter" {
+  owners = ["amazon"]
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami = data.aws_ami.iris_ec2_ami_filter.id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  # Use this for your user data (script from top to bottom)
+  # install httpd (Linux 2 version)
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "web_instance"
+  }
+
+  volume_tags = {
+    Name = "web_instance"
+  } 
 }
