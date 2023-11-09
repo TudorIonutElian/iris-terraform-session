8ab684f0db4eac8b944f5f78406a9972020b32d8
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-09 08:52:52 +0200

    remove sec group

--- main.tf
+++ main.tf
@@ -16,42 +16,6 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_security_group" "sg" {
-  name        = "allow_ssh_http"
-  description = "Allow ssh http inbound traffic"
-  vpc_id      = aws_vpc.app_vpc.id
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh_http"
-  }
-}
-
 # Added data source to filter the ami
 data "aws_ami" "iris_ec2_ami_filter" {
   owners = ["amazon"]
@@ -72,7 +36,6 @@ data "aws_ami" "iris_ec2_ami_filter" {
 resource "aws_instance" "web" {
   ami = data.aws_ami.iris_ec2_ami_filter.id
   instance_type   = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
 
   user_data = <<-EOF
   #!/bin/bash
