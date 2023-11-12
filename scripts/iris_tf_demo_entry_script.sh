b6fdda8979eed1d660dfcd9c7038794d908de69c
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-12 22:32:48 +0200

    Add head style

--- scripts/iris_tf_demo_entry_script.sh
+++ scripts/iris_tf_demo_entry_script.sh
@@ -15,9 +15,10 @@ echo '<html>' >> index.html
 echo '<head>' >> index.html
 echo '<title>Simple Ec2 instance on Iris Begginers TF Demo</title>' >> index.html
 echo '<meta charset="UTF-8">' >> index.html
+echo '<style>body div {display:flex; flex-direction: row; align-items: center; justify-content: center; background-color: #001f3f; color: #ffffff; font-weight:bold;} </style>'
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<div style="display:flex; flex-direction: row; align-items: center; justify-content: center; background-color: #001f3f; color: #ffffff; font-weight:bold;">' >> index.html
+echo '<div>' >> index.html
 echo '<h1>Welcome to Iris Terraform Demo</h1>' >> index.html
 echo '<h3>Hope you enjoyed!</h3>' >> index.html
 echo '</div>' >> index.html
