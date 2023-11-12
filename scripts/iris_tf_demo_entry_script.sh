49f3def458c05592ca822c7feba2e2d33fcfe944
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-12 22:20:47 +0200

    Update script

--- scripts/iris_tf_demo_entry_script.sh
+++ scripts/iris_tf_demo_entry_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+d#!/bin/bash
 # package updates
 #!/bin/bash
   # Use this for your user data (script from top to bottom)
@@ -17,7 +17,9 @@ echo '<title>Level It Up</title>' >> index.html
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
+echo '<div style="display:flex; flex-direction: row; align-items: center; justify-content: center; background-color: #001f3f; color: #ffffff; font-weight:bold;">' >> index.html
 echo '<h1>Welcome to Iris Terraform Demo</h1>' >> index.html
 echo '<h3>Hope you enjoyed!</h3>' >> index.html
-echo '</body>' >> index.html
+echo '</div>'
+echo '</div>' >> index.html
 echo '</html>' >> index.html
