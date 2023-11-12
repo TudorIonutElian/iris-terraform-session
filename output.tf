06b98989e93fc882452ca48bbe22bb6f6ef35fda
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-12 22:11:38 +0200

    Fix instance name for output

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "web_instance_ip" {
     description = "IP Value for public website"
-    value = "http://${aws_instance.web.public_ip}/index.html"
+    value = "http://${aws_instance.iris_tf_demo_ec2_instance.public_ip}/index.html"
 }
