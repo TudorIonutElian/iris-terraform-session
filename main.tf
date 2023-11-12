b7c076daa571e50f7731be14081e9f5d38291e9d
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-12 22:10:44 +0200

    Update some data from variable and add comments

--- main.tf
+++ main.tf
@@ -8,17 +8,23 @@ terraform {
   }
 }
 
-# Add configuration to authorisation keys
-# Configure the AWS Provider
+/**********************************************************
+  # Add configuration to authorisation keys
+  # Configure the AWS Provider  
+**********************************************************/
+
 provider "aws" {
   shared_config_files = ["~/.aws/config"]
   shared_credentials_files = [ "~/.aws/credentials" ]
   profile = "default"
 }
 
-# Added data source to filter the ami
+/**********************************************************
+  # Added data source to filter the ami called iris_tf_demo_ec2_ami_filter
+**********************************************************/
+
 data "aws_ami" "iris_tf_demo_ec2_ami_filter" {
-  owners = ["amazon"]
+  owners = [var.ami_owner]
   most_recent = true
 
   filter {
@@ -32,23 +38,24 @@ data "aws_ami" "iris_tf_demo_ec2_ami_filter" {
   }
 }
 
+/**********************************************************
+  # Create key-pair called iris_tf_demo_key_pair
+**********************************************************/
 resource "aws_key_pair" "iris_tf_demo_key_pair" {
   key_name   = "deployer-key"
   public_key = file("auth_keys/iris-tf-demo.pub")
 }
 
-
-resource "aws_instance" "web" {
+/**********************************************************
+  # Create Ec2 Instance called iris_tf_demo_ec2_instance
+**********************************************************/
+resource "aws_instance" "iris_tf_demo_ec2_instance" {
   ami = data.aws_ami.iris_tf_demo_ec2_ami_filter.id
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type
   key_name = aws_key_pair.iris_tf_demo_key_pair.key_name
-  user_data = file("scripts/iris_tf_demo_entry_script.sh")
+  user_data = file(var.entry_script_path)
 
   tags = {
-    Name = "web_instance"
+    Name = "iris_terraform_demo_ec2_instance"
   }
-
-  volume_tags = {
-    Name = "web_instance"
-  } 
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable instance_type {
+    description = "Value for the ec2 instance type"
+    default = "t2.micro"
+}
+
+variable entry_script_path {
+    description = "Path value for entry script"
+    default = "scripts/iris_tf_demo_entry_script.sh"
+}
+
+variable "ami_owner" {
+    description = "Value for AMI owner"
+    default = "amazon"
+  
+}
