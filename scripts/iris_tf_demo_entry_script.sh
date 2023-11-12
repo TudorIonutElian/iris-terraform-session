2bf588968a3d556a136d979830433abf6b0e9e72
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-12 22:25:25 +0200

    Fix entry script

--- scripts/iris_tf_demo_entry_script.sh
+++ scripts/iris_tf_demo_entry_script.sh
@@ -13,11 +13,11 @@ cd /var/www/html/
 echo '<!DOCTYPE html>' > index.html
 echo '<html>' >> index.html
 echo '<head>' >> index.html
-echo '<title>Level It Up</title>' >> index.html
+echo '<title>Simple Ec2 instance on Iris Begginers TF Demo</title>' >> index.html
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<div style="display:flex; flex-direction: row; align-items: center; justify-content: center; background-color: #001f3f; color: #ffffff; font-weight:bold;">' >> index.html
+echo '<div>' >> index.html
 echo '<h1>Welcome to Iris Terraform Demo</h1>' >> index.html
 echo '<h3>Hope you enjoyed!</h3>' >> index.html
 echo '</div>'
