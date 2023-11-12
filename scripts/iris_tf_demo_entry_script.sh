5e2f73654a1cecf983635367bcf76e372124545c
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-12 22:28:03 +0200

    Try script

--- scripts/iris_tf_demo_entry_script.sh
+++ scripts/iris_tf_demo_entry_script.sh
@@ -17,9 +17,9 @@ echo '<title>Simple Ec2 instance on Iris Begginers TF Demo</title>' >> index.htm
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<div>' >> index.html
+echo '<div style="display:flex; flex-direction: row; align-items: center; justify-content: center; background-color: #001f3f; color: #ffffff; font-weight:bold;">' >> index.html
 echo '<h1>Welcome to Iris Terraform Demo</h1>' >> index.html
 echo '<h3>Hope you enjoyed!</h3>' >> index.html
-echo '</div>'
+echo '</div>' >> index.html
 echo '</div>' >> index.html
 echo '</html>' >> index.html
