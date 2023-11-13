1efaad0c45c2af10a761adfcd47ec9797ba1f32d
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-13 21:03:58 +0200

    Adding setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "**************************************************"
+echo -e "****** Starting Iris Terraform Demo Script ******"
+echo -e "**************************************************"
+
+
+# Connecting through ssh
+ssh -i iris_terraform_demo root@80.240.29.30
