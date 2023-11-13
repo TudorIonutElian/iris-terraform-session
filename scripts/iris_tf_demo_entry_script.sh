ae3d7ea9972b58cf02a1d4b54c2c6e024da91e83
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-13 20:54:18 +0200

    Make it flex columns

--- scripts/iris_tf_demo_entry_script.sh
+++ scripts/iris_tf_demo_entry_script.sh
@@ -16,7 +16,7 @@ echo '<title>Simple Ec2 instance on Iris Begginers TF Demo</title>' >> index.htm
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<div style="display:flex; flex-direction:row; align-items: center; justify-content: center; background-color: blue; color: #fff;">' >> index.html
+echo '<div style="display:flex; flex-direction:columns; align-items: center; justify-content: center; background-color: blue; color: #fff;">' >> index.html
 echo '<div><h1>Welcome to Iris Terraform Demo</h1></div>' >> index.html
 echo '<div><h3>Hope you enjoyed!</h3></div>' >> index.html
 echo '</div>' >> index.html
