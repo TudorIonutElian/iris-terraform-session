312cead8ea2680fd3f8309db6f854bda60872b2a
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2023-11-19 13:18:47 +0200

    Instruct entry to clone git

--- scripts/iris_tf_demo_entry_script.sh
+++ scripts/iris_tf_demo_entry_script.sh
@@ -5,20 +5,12 @@
   # install httpd (Linux 2 version)
 yum update -y
 yum install -y httpd
+yum install -y git
 systemctl start httpd
 systemctl enable httpd
 
 cd /var/www/html/
-echo '<!DOCTYPE html>' > index.html
-echo '<html>' >> index.html
-echo '<head>' >> index.html
-echo '<title>Simple Ec2 instance on Iris Begginers TF Demo</title>' >> index.html
-echo '<meta charset="UTF-8">' >> index.html
-echo '</head>' >> index.html
-echo '<body>' >> index.html
-echo '<div style="display:flex; flex-direction:columns; align-items: center; justify-content: center; background-color: blue; color: #fff;">' >> index.html
-echo '<div>Welcome to Iris Terraform Demo</div>' >> index.html
-echo '<div>Hope you enjoyed!</div>' >> index.html
-echo '</div>' >> index.html
-echo '</body>' >> index.html
-echo '</html>' >> index.html
+
+git clone https://github.com/TudorIonutElian/sample-iris-demo-website.git
+
+cp sample-iris-demo-website/index.html /var/www/html/index.html
